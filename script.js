8e2fc33e55beb1e9272023ff13da66e3fef2d4bc
DannyM2027 <daniel.matut2027@gbwl.org>
2025-03-03 20:06:27 +0000

    day 1

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log("hello world");
+/* This is a multi line comment
+below is an example JS statement (statement is instructiion/command)
+Every sattement ends with a semicolon (;)
+console.log is a function that displays info 
+*/
+
+console.log("screw matthew");
+
+// This is a single line comment
+
+
+// Declare a new variable to hold some data
+let pokemon; 
+
+// ASSIGN a Value to that variable
+// like putting an item in a box
+pokemon = "Squirtle" ;
+// Declare + Assign in one statement
+let pokemon1 = "squirtle";
